bc5d5560343ff101134c5d3e5a3660ae0d329c01
GLBryan <garrettleebryan@gmail.com>
2021-10-22 11:15:14 -0400

    added prompts

---
+++ index.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const inq = require('inquirer')
+
+const prompts = [
+    {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "title",
+    },
+    {
+        type: "input",
+        message: "What is a brief description of your project?",
+        name: "desc",
+    },
+    {
+        type: "input",
+        message: "What are the installation instructions of your application?",
+        name: "install",
+    },
+    {
+        type: "input",
+        message: "What is the usage information for your project?",
+        name: "usage",
+    },
+    {
+        type: "input",
+        message: "What are the contribution guidelines for your project?",
+        name: "contrib",
+    },
+    {
+        type: "input",
+        message: "What are the test instructions for your project?",
+        name: "test",
+    }
+]
+
+inq
+    .prompt(prompts)
